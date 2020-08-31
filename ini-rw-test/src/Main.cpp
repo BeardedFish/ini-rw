9282262e56327916af979d49aaac92dd2ee8e287
Darian Benam <darianbenam@hotmail.com>
2020-08-31 09:40:37 -0400

    Ported some of the functionality to the test project from the old source

--- ini-rw-test/src/Main.cpp
+++ ini-rw-test/src/Main.cpp
@@ -4,6 +4,7 @@
 
 #include "ini-rw/IniSetting.hpp"
 #include <iostream>
+#include <string>
 
 const char* INI_RW_ACSII_ART = " _            _                              \n"
                                "|_|          |_|                             \n"
@@ -18,19 +19,66 @@ const char* INI_RW_ACSII_ART = " _            _                              \n"
 /// <summary>
 /// Main entry point of the program.
 /// </summary>
+/// <param name="argc"></param>
+/// <param name="argv"></param>
 /// <returns>An integer that represents the exit code status of the program.</returns>
-int main()
+int main(int argc, char* argv[])
 {
-	std::cout << INI_RW_ACSII_ART << std::endl;
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-	IniRW::IniSetting iniSettings("..\\example-ini-files\\example1.ini");
-    
+    std::cout << INI_RW_ACSII_ART << std::endl << std::endl;
+
+    if (argc <= 1)
+    {
+        std::cerr << "Invalid amount of parameters." << std::endl;
+        std::cerr << "Usage: " << (argc == 0 ? "[EXECUTABLE_NAME]" : argv[0]) << " [INI_FILE_PATH]" << std::endl;
+
+        return EXIT_FAILURE;
+    }
+
+    IniRW::IniSetting iniSettings(argv[1]);
+
     if (iniSettings.IsLoaded())
     {
-        std::cout << iniSettings.ToString() << std::endl;
+        std::cout << "The INI file \"" << argv[1] << "\" was loaded succesfully!" << std::endl << std::endl;
+
+        bool exitLoopFlag = false;
+        std::string userInput;
+
+        do
+        {
+            std::cout << ">> ";
+            std::getline(std::cin, userInput);
+
+            exitLoopFlag = userInput == "quit";
+
+            // TODO: Make user input case insensitive...
+
+            if (userInput != "quit")
+            {
+                if (userInput == "contents")
+                {
+                    std::string iniContents = iniSettings.ToString();
+
+                    std::cout << (iniContents.empty() ? "[!] FILE IS EMPTY" : iniContents);
+                }
+                else if (userInput == "save")
+                {
+                    bool success = iniSettings.SaveChanges();
+
+                    std::cout << (success ? "The INI file was saved succesfully!" : "An error occured while trying to save the INI file.");
+                }
+                else
+                {
+                    std::cout << "Invalid command!";
+                }
+
+                std::cout << std::endl << std::endl;
+            }
+        } while (!exitLoopFlag);
     }
     else
     {
-        std::cout << "Fail";
+        std::cout << "An error occured while trying to load the file \"" << argv[1] << "\". Program will now terminate..." << std::endl << std::endl;
     }
 }
