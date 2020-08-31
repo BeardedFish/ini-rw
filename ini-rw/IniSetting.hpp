bc8da46afdb02027187a60d0b036b66c2fad2d93
Darian Benam <darianbenam@hotmail.com>
2020-08-31 02:42:42 -0400

    Added comments to the IniSetting.hpp file

--- ini-rw/IniSetting.hpp
+++ ini-rw/IniSetting.hpp
@@ -19,12 +19,30 @@ namespace IniRW
 		std::vector<IniEntity*> iniContents;
 
 	public:
+		/// <summary>
+		/// Constructor for creating the IniSetting class. By calling this constructor, the INI file passed in the parameters is loaded in the class.
+		/// </summary>
+		/// <param name="iniFilePath">The path to the INI file to be loaded in the IniSetting class.</param>
+		/// <returns>Nothing. To make sure that the INI file was loaded succesfully check the IsLoaded() function.</returns>
 		IniSetting(const std::string& iniFilePath);
 
+		/// <summary>
+		/// Returns a boolean that states whether the INI file was loaded succesfully or not.
+		/// </summary>
+		/// <returns>True if the INI file was loaded succesfully, if not, false.</returns>
 		bool IsLoaded() const;
+
+		/// <summary>
+		/// Gets the file contents of the loaded INI file as a string.
+		/// </summary>
+		/// <returns>A string which contains the contents of the loaded INI file.</returns>
 		std::string ToString();
 
 	private:
+		/// <summary>
+		/// Loads an INI file's contents into the current instance of the class.
+		/// </summary>
+		/// <param name="iniFilePath">The path to the INI file.</param>
 		void LoadIniFile(const std::string& iniFilePath);
 
 	};
