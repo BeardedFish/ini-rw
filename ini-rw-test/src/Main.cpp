24e60b022a02fccd01dbf216153d6cb5a071fe68
Darian Benam <sonic8662@gmail.com>
2020-09-01 20:38:28 -0400

    Used boolean variable in if statement

--- ini-rw-test/src/Main.cpp
+++ ini-rw-test/src/Main.cpp
@@ -110,7 +110,7 @@ int main(int argc, char* argv[])
                 std::cout << "Input cannot be empty!";
             }
 
-            if (userInputTokens[0] != "quit")
+            if (exitLoopFlag)
             {
                 std::cout << std::endl << std::endl;
             }
