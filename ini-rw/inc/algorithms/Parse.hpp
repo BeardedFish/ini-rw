c817ea70b75f417654ee3f11c5029aa06690df64
Darian Benam <darianbenam@hotmail.com>
2020-09-03 13:49:33 -0400

    Added documentation to the "Parse.hpp" file

--- ini-rw/inc/algorithms/Parse.hpp
+++ ini-rw/inc/algorithms/Parse.hpp
@@ -9,7 +9,23 @@
 
 namespace IniRW
 {
+	/// <summary>
+	/// Extracts the section name from an INI section by ignoring the leading [ and ending ] and extracting the string inbetween these two characters.
+	/// </summary>
+	/// <param name="section">The string that contains the formatted INI section.</param>
+	/// <returns>
+	/// If the string is a valid INI section, then the extracted INI section name is returned. If it is not a valid section, then the original string is returned.
+	/// </returns>
 	std::string ExtractSectionName(const std::string& section);
 
+	/// <summary>
+	/// Searches a string for an INI comment and if it finds one, it extracts it and removes it from the INI value string. A valid INI comment begins with any character
+	/// defined in the "commentPrefixes" vector. Escaped comments (ex: \# or \;) are ignored.
+	/// </summary>
+	/// <param name="commentPrefixes">A vector of type char which contains all the valid INI comment prefixes.</param>
+	/// <param name="value">The string to be modified and parsed.</param>
+	/// <returns>
+	/// If a comment exists in the value, then a string containing the extracted comment is returned. If no comment exists in the value, then an empty string is returned.
+	/// </returns>
 	std::string ExtractAndRemoveComment(const std::vector<char>& commentPrefixes, std::string& value);
 }
