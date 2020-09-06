63f401146c4db425657f4eba36662e0ff7cc832d
Darian Benam <darianbenam@hotmail.com>
2020-09-05 23:14:08 -0400

    Removed deconstructor for the "IniSection" class

--- ini-rw/src/entities/IniSection.cpp
+++ ini-rw/src/entities/IniSection.cpp
@@ -12,11 +12,6 @@ namespace IniRW
 		this->name = name;
 	}
 
-	IniSection::~IniSection()
-	{
-		// TODO: Deallocate the right side data if it exists
-	}
-
 	IniEntityType IniSection::GetType()
 	{
 		return IniEntityType::Section;
