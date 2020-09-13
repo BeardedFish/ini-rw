cec4d190dae6e4408a9aa90d6867664f969aaa52
Darian Benam <darianbenam@hotmail.com>
2020-09-12 21:36:50 -0400

    Added function documentation to the "Validation.hpp" file

--- ini-rw/inc/algorithms/Validation.hpp
+++ ini-rw/inc/algorithms/Validation.hpp
@@ -9,5 +9,11 @@
 
 namespace IniRW
 {
+	/// <summary>
+	/// Compares two strings for equality, ignoring case.
+	/// </summary>
+	/// <param name="str1">The string to be compared to the second string.</param>
+	/// <param name="str2">The string to be compared to the first string.</param>
+	/// <returns>True if the strings are equal (ignoring case), if not, false.</returns>
 	bool EqualsIgnoreCase(const std::string& str1, const std::string& str2);
 }
