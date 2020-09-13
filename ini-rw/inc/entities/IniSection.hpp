0a22f9d9a1901dae047d849c3349493f6206f2bc
Darian Benam <darianbenam@hotmail.com>
2020-09-13 17:50:29 -0400

    Added XML comment documentation to the "IniSection.hpp" file

--- ini-rw/inc/entities/IniSection.hpp
+++ ini-rw/inc/entities/IniSection.hpp
@@ -18,19 +18,53 @@ namespace IniRW
 		IniValueCommentPair extraData;
 
 	public:
+		/// <summary>
+		/// Creates an INI section object with a custom name.
+		/// </summary>
+		/// <param name="name">The name of the INI section.</param>
 		IniSection(const std::string& name);
 
+		/// <summary>
+		/// Creates an INI section object with leading whitespace, a custom name, and extra data.
+		/// </summary>
+		/// <param name="whitespace">The string which contains whitespace. If the string does not contain whitespace, an std::exception is thrown.</param>
+		/// <param name="name">The name of the INI section.</param>
+		/// <param name="extraData">The extra data attached to the INI section. This can either be garbage data or an inline INI comment.</param>
 		IniSection(const std::string& leadingWhitespace, const std::string& name, IniValueCommentPair extraData);
 
+		/// <summary>
+		/// Gets an enum that represents the type of INI entity this object is.
+		/// </summary>
+		/// <returns>The return value will always be IniEntityType::Section.</returns>
 		IniEntityType GetType() const override;
 
+		/// <summary>
+		/// Converts the current state of this object to a formatted string. Refer to "Returns" for the format.
+		/// </summary>
+		/// <returns>
+		/// A string in the format: "[LEADING_WHITESPACE][SECTION_NAME][EXTRA_DATA]" (excluding quotes). The SECTION_NAME includes a leading "[" character  and a trailing
+		/// "]" character.
+		/// </returns>
 		std::string ToString() const override;
 
+		/// <summary>
+		/// Gets the name of this INI section.
+		/// </summary>
+		/// <returns>A string which contains the name of this INI key.</returns>
 		std::string GetName() const;
 
+		/// <summary>
+		/// Sets the leading whitespace for this INI section instance. Leading whitespace appears before the INI section name.
+		/// </summary>
+		/// <param name="whitespace">The string which contains whitespace. If the string does not contain whitespace, an std::exception is thrown.</param>
 		void SetLeadingWhitespace(const std::string& whitespace);
 
 	private:
+		/// <summary>
+		/// States whether a string contains only whitespace or not.
+		/// </summary>
+		/// <param name="str">The string to be evaluated.</param>
+		/// <returns>True if the string contains only whitespace, if not, false.</returns>
 		bool IsWhitespace(const std::string& str);
 	};
 }
